f2a0179b6725aee3bc25ce3672acf652fa5c8207
small_ho <shuanghaox@163.com>
2022-04-14 17:18:49 +0800

    测试jitpack发布

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dlopkit
-test tool library
+
+#### 介绍
+android开发测试工具
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1.  xxxx
+2.  xxxx
+3.  xxxx
+
+#### 使用说明
+
+1.  xxxx
+2.  xxxx
+3.  xxxx
+
+#### 发布说明
+1. 创建github账号，创建项目
+2. 点击标签--->点击发行版---->点击创建发行版--->创建
+3. 打开jitpack网址，将https地址复制进入---完成
--- corekit/src/main/java/com/small/corekit/Ceshi.kt
+++ corekit/src/main/java/com/small/corekit/Ceshi.kt
@@ -7,8 +7,8 @@ import android.widget.Toast
  * Created by small-ho on 2022/04 15:58
  * title:
  */
-class Ceshi {
+object Ceshi {
     fun text(context: Context) {
-        Toast.makeText(context, "卡楼呀" ,Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "哈喽" ,Toast.LENGTH_SHORT).show()
     }
 }
