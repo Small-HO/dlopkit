b176482b93b6eaf3d112a8c4fbfb748aa9f827cf
small_ho <shuanghaox@163.com>
2022-04-14 16:09:28 +0800

    第一次提交

---
+++ corekit/src/main/java/com/small/corekit/Ceshi.kt
@@ -0,0 +1,14 @@
+package com.small.corekit
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Created by small-ho on 2022/04 15:58
+ * title:
+ */
+class Ceshi {
+    fun text(context: Context) {
+        Toast.makeText(context, "卡楼呀" ,Toast.LENGTH_SHORT).show()
+    }
+}
