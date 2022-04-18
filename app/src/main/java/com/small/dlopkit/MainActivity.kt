cf94850859170120f969feae3637a40dbd3e7bb6
small_ho <shuanghaox@163.com>
2022-04-18 10:16:11 +0800

    测试发布后的库使用

--- app/src/main/java/com/small/dlopkit/MainActivity.kt
+++ app/src/main/java/com/small/dlopkit/MainActivity.kt
@@ -2,10 +2,23 @@ package com.small.dlopkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.small.floatkit.common.DlopConfig
+import com.small.floatkit.manager.PopDlopManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PopDlopManager.init(this, DlopConfig().apply {
+            http_dev = "http://www.baidu.com"
+            http_main = "http://github.com"
+        })
+
+        findViewById<Button>(R.id.ceshi).setOnClickListener {
+            Log.e("测试------->", PopDlopManager.getHttpUrl())
+        }
+
     }
 }
