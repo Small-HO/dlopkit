7ae4602e401b9519e2135a5185035b6d548148aa
small_ho <shuanghaox@163.com>
2022-06-28 11:32:56 +0800

    修改异常问题

--- editorkit/src/main/java/com/small/editorkit/RichUtils.kt
+++ editorkit/src/main/java/com/small/editorkit/RichUtils.kt
@@ -17,13 +17,13 @@ object RichUtils {
         if (TextUtils.isEmpty(content)) {
             return imageSrcList as ArrayList<String?>
         }
-        val p = Pattern.compile("<img\\b[^>]*\\bsrc\\b\\s*=\\s*('|\")?([^'\"\n\r>]+(\\.jpg|\\.bmp|\\.eps|\\.gif|\\.mif|\\.miff|\\.png|\\.tif|\\.tiff|\\.svg|\\.wmf|\\.jpe|\\.jpeg|\\.dib|\\.ico|\\.tga|\\.cut|\\.pic|\\b)\\b)[^>]*>", Pattern.CASE_INSENSITIVE)
+        val p = Pattern.compile("<img\\b[^>]*\\bsrc\\b\\s*=\\s*(['\"])?([^'\"\n\r>]+(\\.jpg|\\.bmp|\\.eps|\\.gif|\\.mif|\\.miff|\\.png|\\.tif|\\.tiff|\\.svg|\\.wmf|\\.jpe|\\.jpeg|\\.dib|\\.ico|\\.tga|\\.cut|\\.pic|\\b)\\b)[^>]*>", Pattern.CASE_INSENSITIVE)
         val m = p.matcher(content)
         var quote: String?
         var src: String?
         while (m.find()) {
             quote = m.group(1)
-            src = if (quote == null || quote.trim { it <= ' ' }.isEmpty()) m.group(2).split("//s+".toRegex()).toTypedArray()[0] else m.group(2)
+            src = if (quote == null || quote.trim { it <= ' ' }.isEmpty()) m.group(2)?.split("//s+".toRegex())?.toTypedArray()?.get(0) else m.group(2)
             imageSrcList.add(src)
         }
         return imageSrcList as ArrayList<String?>
