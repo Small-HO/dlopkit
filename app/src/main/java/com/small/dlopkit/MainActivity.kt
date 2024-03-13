1e8f4859e6103ca2442a4ab9e03b5bba6a9ec0f3
small_ho <shuanghaox@163.com>
2024-03-13 17:12:06 +0800

    去除多余资源

--- app/src/main/java/com/small/dlopkit/MainActivity.kt
+++ app/src/main/java/com/small/dlopkit/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.small.dlopkit
 
-import android.Manifest
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.SurfaceView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.hjq.permissions.Permission
-import com.hjq.permissions.XXPermissions
-import com.small.camerakit.Matisse
-import com.small.camerakit.MimeType
-import com.small.camerakit.engine.impl.GlideEngine
-import com.small.camerakit.internal.entity.CaptureStrategy
 import com.small.editorkit.RichEditor
 import com.small.floatkit.common.DlopConfig
 import com.small.floatkit.manager.PopDlopManager
-import com.small.videokit.FFmpegNativeUtils.startVideo
-import com.small.videokit.FFmpegNativeUtils.videoPlay
 
 class MainActivity : AppCompatActivity() {
     
@@ -32,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnMulti).setOnClickListener {
-//            startActivity(Intent(this,MultiActivity::class.java))
         }
 
         PopDlopManager.init(this, DlopConfig().apply {
@@ -72,21 +60,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btCar).setOnClickListener {
-            XXPermissions.with(this)
-                .permission(Permission.MANAGE_EXTERNAL_STORAGE)
-                .permission(Manifest.permission.CAMERA)
-                .request { _, allGranted ->
-                    if (allGranted) {
-                        Matisse.from(this)
-                            .choose(MimeType.ofImage())           // 图片类型
-                            .countable(true)           // true:选中后显示数字;false:选中后显示对号
-                            .maxSelectable(1)      // 可选的最大数
-                            .capture(false)              // 选择照片时，是否显示拍照
-                            .captureStrategy(CaptureStrategy(true, "com.xbkaoyan.ikaoyaner.provider")) //参数1 true表示拍照存储在共有目录，false表示存储在私有目录；参数2与 AndroidManifest中authorities值相同，用于适配7.0系统 必须设置
-                            .imageEngine(GlideEngine()) //图片加载引擎
-                            .forResult(2) //
-                    }
-                }
+
         }
     }
 
